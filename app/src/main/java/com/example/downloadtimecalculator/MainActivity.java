644ad9573fb630f9732a82509269765d823fe790
Rizzo1812 <rizzo.antonioa7000@gmail.com>
2021-10-22 16:25:19 +0200

    added basic widgets

--- app/src/main/java/com/example/downloadtimecalculator/MainActivity.java
+++ app/src/main/java/com/example/downloadtimecalculator/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.downloadtimecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ProgressBar progressBar1 = findViewById(R.id.progressBar1);
+
+        ValueAnimator animator = ValueAnimator.ofInt(0, progressBar1.getMax());
+        animator.setDuration(10000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation){
+                progressBar1.setProgress((Integer)animation.getAnimatedValue());
+            }
+        });
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                // start your activity here
+            }
+        });
+        animator.start();
     }
+
 }
