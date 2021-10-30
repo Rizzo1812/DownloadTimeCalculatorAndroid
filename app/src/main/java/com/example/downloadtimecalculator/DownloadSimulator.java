ca86ff72a8316fccff7c84c5b75ec76b3a6d235f
Antonio Rizzo <rizzo.antonioa7000@gmail.com>
2021-10-30 20:28:07 +0200

    Developed most of the app cycle

--- app/src/main/java/com/example/downloadtimecalculator/DownloadSimulator.java
+++ app/src/main/java/com/example/downloadtimecalculator/DownloadSimulator.java
@@ -12,18 +12,17 @@ import android.widget.TextView;
 import java.util.Locale;
 
 public class DownloadSimulator{
-    View view;
-    Activity mainActivity;
-    ProgressBar progressBar;
-    EditText editTextBandwidth;
-    TextView textSpeed;
-    TextView textTime;
-    TextView textDownloaded;
-    TextView textPercentage;
-    int secondsToDownload;
+    private final Activity mainActivity;
+    private final ProgressBar progressBar;
+    private final EditText editTextBandwidth;
+    private final TextView textSpeed;
+    private final TextView textTime;
+    private final TextView textDownloaded;
+    private final TextView textPercentage;
+    private int secondsToDownload;
+    public boolean ready;
 
     public DownloadSimulator(View view,Activity mainActivity){
-        this.view = view;
         this.mainActivity = mainActivity;
         this.progressBar = view.findViewById(R.id.progressBar);
         this.editTextBandwidth = view.findViewById(R.id.editTextBandwidth);
@@ -35,6 +34,7 @@ public class DownloadSimulator{
     }
 
     public void reset(){
+        ready = false;
         progressBar.setProgress(0);
         editTextBandwidth.setText("");
         textSpeed.setText("");
@@ -45,19 +45,25 @@ public class DownloadSimulator{
     }
 
     public void calcDownloadTime(){
+        if(!isSpeedSet()){
+            reset();
+            return;
+        }
         double fileSize = Double.parseDouble(((EditText) (mainActivity.findViewById(R.id.editTxtFileSize))).getText().toString());
         int selectedRadioID = ((RadioGroup) (mainActivity.findViewById(R.id.radioGroupSize))).getCheckedRadioButtonId();
         fileSize = convertToB(fileSize, ((RadioButton) (mainActivity.findViewById(selectedRadioID))).getText().toString());
         double bandwidth = Double.parseDouble(editTextBandwidth.getText().toString());
-        textSpeed.setText(String.format(Locale.getDefault(),"%.3f",bandwidth/8));
         selectedRadioID = ((RadioGroup) (mainActivity.findViewById(R.id.radioGroupBandwidth))).getCheckedRadioButtonId();
+        textSpeed.setText(String.format(Locale.getDefault(),"%.3f %s",bandwidth/8, ((RadioButton) (mainActivity.findViewById(selectedRadioID))).getText().toString().toUpperCase()));
         bandwidth = convertToBps(bandwidth, ((RadioButton) (mainActivity.findViewById(selectedRadioID))).getText().toString());
         if (bandwidth != 0) {
             secondsToDownload = (int) Math.floor(fileSize / bandwidth);
             textTime.setText(timeFormat(secondsToDownload));
         } else {
+            secondsToDownload = -1;
             textTime.setText("∞");
         }
+        ready = true;
     }
 
     public void simulate(){
@@ -93,5 +99,9 @@ public class DownloadSimulator{
         int minutes = (seconds % 3600) / 60;
         seconds = seconds % 60;
         return String.format(Locale.getDefault(),"%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    private boolean isSpeedSet(){
+        return !editTextBandwidth.getText().toString().equals("");
     }
 }
--- app/src/main/java/com/example/downloadtimecalculator/MainActivity.java
+++ app/src/main/java/com/example/downloadtimecalculator/MainActivity.java
@@ -5,21 +5,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout layout;
+    EditText editTxtFileSize;
+    Switch switchRunning;
+    Button buttonCalc;
+    FloatingActionButton floatingAddSimulator;
     LinkedList<DownloadSimulator> listOfDownloadSimulators = new LinkedList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         layout =  findViewById(R.id.main_activity_layout);
+        editTxtFileSize = findViewById(R.id.editTxtFileSize);
+        buttonCalc = findViewById(R.id.buttonCalc);
+        floatingAddSimulator = findViewById(R.id.floatingAddSimulator);
+        addNewDownloader();
+
+        switchRunning = (Switch)  findViewById(R.id.switchRunning);
+        switchRunning.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    buttonCalc.setEnabled(false);
+                    floatingAddSimulator.setEnabled(false);
+                    for (DownloadSimulator simulator : listOfDownloadSimulators) {
+                        simulator.simulate();
+                    }
+                }else{
+                    for (DownloadSimulator simulator : listOfDownloadSimulators) {
+                        simulator.reset();
+                        switchRunning.setEnabled(false);
+                    }
+                    floatingAddSimulator.setEnabled(true);
+                    buttonCalc.setEnabled(true);
+                }
+            }
+        });
+
         /*
         ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar1);
 
@@ -55,8 +93,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnCalcClick(View view) {
-        for (DownloadSimulator simulator : listOfDownloadSimulators) {
-            simulator.calcDownloadTime();
+        if(editTxtFileSize.getText().toString().equals("")){
+            Toast.makeText(this, "Insert file size!",
+                    Toast.LENGTH_LONG).show();
+        }else{
+            for (DownloadSimulator simulator : listOfDownloadSimulators) {
+                simulator.calcDownloadTime();
+                if(simulator.ready) switchRunning.setEnabled(true);
+            }
         }
     }
 }
